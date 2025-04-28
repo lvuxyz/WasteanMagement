e56183bfff5195959b7b924b7bb3b77cd314c978
lvubyte <lvu.byte@gmail.com>
2025-04-29 01:16:43 +0700

    Refactor: Update waste type model and repository
    - Update WasteType model to include `recyclable`, `handlingInstructions`, and `unitPrice` fields.
    - Update WasteType model to be able to parse data from json response.
    - Remove unnecessary fields from WasteType model.
    - Update WasteTypeRepository to fetch data from an external API.
    - Update WasteTypeRepository to support getting WasteType by ID.
    - Update WasteTypeRepository to add `baseUrl`.
    - Update WasteTypeRepository to remove dummy data.
    - Update WasteTypeListItem to render the `recyclable`, `handlingInstructions`, and `unitPrice` data.
    - Update WasteTypeManagementScreen to display a list of WasteTypes from API and add filter functionality.
    - Update WasteTypeManagementScreen to add new functions: search, delete and add.
    - Update WasteTypeManagementScreen to refactor into StatefulWidget.

--- lib/models/waste_type_model.dart
+++ lib/models/waste_type_model.dart
@@ -4,31 +4,62 @@ import 'package:equatable/equatable.dart';
 class WasteType extends Equatable {
   final int id;
   final String name;
-  final String category;
   final String description;
+  final bool recyclable;
+  final String handlingInstructions;
+  final double unitPrice;
   final IconData icon;
   final Color color;
-  final String recyclingMethod;
+  final String category;
   final List<String> examples;
-  final int buyingPrice;
   final String unit;
   final String recentPoints;
 
   const WasteType({
-  required this.id,
-  required this.name,
-  required this.category,
-  required this.description,
-  required this.icon,
-  required this.color,
-  required this.recyclingMethod,
-  required this.examples,
-  required this.buyingPrice,
-  required this.unit,
-  required this.recentPoints,
-});
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.recyclable,
+    required this.handlingInstructions,
+    required this.unitPrice,
+    required this.icon,
+    required this.color,
+    required this.category,
+    required this.examples,
+    required this.unit,
+    required this.recentPoints,
+  });
 
+  factory WasteType.fromJson(Map<String, dynamic> json) {
+    return WasteType(
+      id: json['waste_type_id'],
+      name: json['name'],
+      description: json['description'],
+      recyclable: json['recyclable'] == 1,
+      handlingInstructions: json['handling_instructions'],
+      unitPrice: double.parse(json['unit_price'].toString()),
+      icon: Icons.delete_outline,
+      color: Colors.green,
+      category: json['recyclable'] == 1 ? 'Tái chế' : 'Thường',
+      examples: const [],
+      unit: 'kg',
+      recentPoints: '',
+    );
+  }
 
   @override
-  List<Object> get props => [id, name, category, description, icon, color, recyclingMethod, examples, buyingPrice, unit, recentPoints];
+  List<Object> get props => [
+    id, 
+    name, 
+    description, 
+    recyclable, 
+    handlingInstructions, 
+    unitPrice, 
+    icon, 
+    color, 
+    category, 
+    examples, 
+    unit, 
+    recentPoints
+  ];
 }
