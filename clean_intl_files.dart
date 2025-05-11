1e7ba1d7293638c33020645619c368d341e8540c
lvubyte <lvu.byte@gmail.com>
2025-05-11 14:07:54 +0700

    clean unnecessary files

--- clean_intl_files.dart
+++ clean_intl_files.dart
@@ -1,26 +1,28 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 void main() {
-  final String libL10nDir = 'lib/l10n';
-  final String rootL10nDir = 'l10n';
+  const String libL10nDir = 'lib/l10n';
+  const String rootL10nDir = 'l10n';
   
   try {
     // Kiểm tra và xóa file intl_en.arb trong thư mục lib/l10n
     final libIntlFile = File('$libL10nDir/intl_en.arb');
     if (libIntlFile.existsSync()) {
-      print('Xóa file $libL10nDir/intl_en.arb');
+      debugPrint('Xóa file $libL10nDir/intl_en.arb');
       libIntlFile.deleteSync();
     }
     
     // Kiểm tra và xóa file intl_en.arb trong thư mục l10n
     final rootIntlFile = File('$rootL10nDir/intl_en.arb');
     if (rootIntlFile.existsSync()) {
-      print('Xóa file $rootL10nDir/intl_en.arb');
+      debugPrint('Xóa file $rootL10nDir/intl_en.arb');
       rootIntlFile.deleteSync();
     }
-    
-    print('Hoàn tất kiểm tra và xóa các file intl_en.arb không mong muốn');
+
+    debugPrint('Hoàn tất kiểm tra và xóa các file intl_en.arb không mong muốn');
   } catch (e) {
-    print('Lỗi khi xóa file: $e');
+    debugPrint('Lỗi khi xóa file: $e');
   }
 } 
